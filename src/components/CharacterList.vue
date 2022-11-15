6128161f3c926f1e30e17da51e2cb86bc0f9e74f
MicheleBenaco <benaco.michele@yahoo.it>
2022-11-15 16:07:46 +0100

    card

--- src/components/CharacterList.vue
+++ src/components/CharacterList.vue
@@ -1,13 +1,14 @@
 <template>
-    <div class="row">
-        <div class=" col-12 col-sm-6 col-md-4 col-lg-3">
+    <div class="row pt-5">
+        <div class=" col-12 col-sm-6 col-md-4 col-lg-3  p-5" v-for="(item,index) in characters" :key="id" >
 
-            <div class="character" v-for="(item,index) in characters" :key="id">
-                    <img :src="item.img" alt="">
-                    <h3>{{item.name}}</h3>
+            <div class="character ">
+                    <img :src="item.img" alt="img" class="">
+                    <h1>{{item.name}}</h1>
                     <h3>{{item.birthday}}</h3>
                     <h3>{{item.nickname}}</h3>
                     <h3>{{item.status}}</h3>
+                    <h3>{{item.category}}</h3>
             </div>
            
         </div>
@@ -15,16 +16,9 @@
     </div>
 </template>
 
-// "char_id": 1,
-// "name": "Walter White",
-// "birthday": "09-07-1958",
-// "occupation": [
-// "High School Chemistry Teacher",
-// "Meth King Pin"
-// ],
-// "img": "https://images.amcnetworks.com/amc.com/wp-content/uploads/2015/04/cast_bb_700x1000_walter-white-lg.jpg",
-// "status": "Presumed dead",
-// "nickname": "Heisenberg",
+
+
+
 
 <script>
     export default {
@@ -34,5 +28,29 @@
 </script>
 
 <style lang="scss" scoped>
+    .character{
+        text-align: center;
+        h3{
+            padding-top: 5px;
+            font-weight: 700;
+        }
+
+        h1{
+            color: green;
+        }
+       
+
+       
+
+
+        img{
+            width:  250px;
+            height: 250px;
+            border-radius: 50%;
+            object-fit: cover;
+            overflow: hidden;    
+         
+        }
+    }
 
 </style>
